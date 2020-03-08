f4513705c65a0531f5b833239e41b7f115f70070
madking55 <madking55@gmail.com>
2020-03-08 12:28:39 +0100

    Modify nesting ti skip tests

--- data-types/collections/advanced_nested_collections/nesting_test.rb
+++ data-types/collections/advanced_nested_collections/nesting_test.rb
@@ -16,7 +16,6 @@ require 'minitest/pride'
 class NestedTest < MiniTest::Test
 
   def test_list_of_olive_garden_employess
-    skip
     #^^^ Un-Skip each test
     #=======================
     # EXAMPLE
@@ -26,57 +25,50 @@ class NestedTest < MiniTest::Test
   end
 
   def test_pancake_ingredients
-    skip
     #=======================
-    # pancake_ingredients = <your code here>
+    pancake_ingredients = stores[:dennys][:dishes].first[:ingredients]
     #=======================
     assert_equal ["Flour", "Eggs", "Milk", "Syrup"], pancake_ingredients
   end
 
   def test_rissotto_price
-    skip
     #=======================
-    # risotto_price = <your code here>
+    risotto_price = stores[:olive_garden][:dishes].first[:price]
     #=======================
     assert_equal 12, risotto_price
   end
 
   def test_big_mac_ingredients
-    skip
     #=======================
-    # big_mac_ingredients = <your code here>
+    big_mac_ingredients = stores[:macdonalds][:dishes].first[:ingredients]
     #=======================
     assert_equal ['Bun','Hamburger','Ketchup','pickles'], big_mac_ingredients
   end
 
   def test_list_of_restaurants
-    skip
     #=======================
-    # store_names = <your code here>
+    store_names = stores.keys
     #=======================
     assert_equal [:olive_garden, :dennys, :macdonalds], store_names
   end
 
   def test_list_of_dishes_names_for_olive_garden
-    skip
     #=======================
-    # dishes_names = <your code here>
+    dishes_names = stores[:olive_garden][:dishes].map { |dish| dish[:name] }
     #=======================
     assert_equal ['Risotto', 'Steak'], dishes_names
   end
 
   def test_list_of_employees_across_all_restaurants
-    skip
     #=======================
-    # employee_names = <your code here>
+    employee_names = stores.map { |k,v| v[:employees] }.flatten
     #=======================
     assert_equal ["Jeff","Zach","Samantha","Bob","Sue","James","Alvin","Simon","Theodore"], employee_names
   end
 
   def test_list_of_all_ingredients_across_all_restaurants
-    skip
     #=======================
-    # ingredients = <your code here>
+    # ingredients = stores
     #=======================
     assert_equal ["Rice",
                   "Cheese",
