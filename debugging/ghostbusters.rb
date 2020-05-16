93ec30db497961b72cb561fd629d77512a34c876
madking55 <madking55@gmail.com>
2020-05-16 12:09:31 +0200

    Finish debugging

--- debugging/ghostbusters.rb
+++ debugging/ghostbusters.rb
@@ -1,14 +1,14 @@
 class Ghostbuster
 
   def initialize(name)
-    @nme = name
+    @name = name
   end
 
   def say_my_name
-    puts "I'm #{name}"
+    puts "I'm #{@name}"
   end
 
-  def get_car(car == "ECTO-1")
+  def get_car(car = "ECTO-1")
     @car = car
   end
 
@@ -17,5 +17,5 @@ end
 
 ghostbuster = Ghostbuster.new("Ray")
 ghostbuster.say_my_name
-puts ghostbuster.ride
+puts ghostbuster.get_car
 
